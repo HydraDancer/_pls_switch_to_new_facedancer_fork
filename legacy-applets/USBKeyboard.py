5acb50d50b4b6e41627924431ac6d9e05c9fe554
Philippe Teuwen <phil@teuwen.org>
2023-10-18 11:59:50 +0200

    few fixes in legacy

--- legacy-applets/USBKeyboard.py
+++ legacy-applets/USBKeyboard.py
@@ -2,8 +2,6 @@
 #
 # Contains class definitions to implement a USB keyboard.
 
-import greatfet
-
 from facedancer.USB import *
 from facedancer.USBDevice import *
 from facedancer.USBConfiguration import *
